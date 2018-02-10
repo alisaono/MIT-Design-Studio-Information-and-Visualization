74276b446e1882a1f1a8736e5a971143e80df445
Alisa Ono <alisao@mit.edu>
2018-02-10 03:49:55 -0500

    Moved basic constants to shared.js; Refactored battery clock to use p5 instance instead

--- Project_1/script_1.js
+++ Project_1/script_1.js
@@ -1,112 +1,106 @@
-// Time constants
-const HOUR_PER_DAY = 24
-const MIN_PER_HOUR = 60
-const SEC_PER_MIN = 60
+const batteryClock = function (p) {
+  // Headers measurements
+  const headerSize = 28
+  const headerFont = 'Arial'
+  const topHeaderY = 74
+  const btmHeaderY = 132
 
-// Canvas measurements
-const canvasWidth = 412
-const canvasHeight = 734
+  // Battery measurements
+  const btryHeight = 120
+  const btryWidth = 300
+  const btryX = 54
+  const btryInitY = 194
+  const btryPad = 60
+  const btryRad = 10
+  const btryStroke = 2
+  const cthdHeight = 50
+  const cthdWidth = 10
+  const cthdRadius = 1
+  const barRadius = 3
 
-// Headers measurements
-const headerSize = 28
-const headerFont = 'Arial'
-const topHeaderY = 74
-const btmHeaderY = 132
+  // Charge thresholds
+  const lowCharge = 0.2
+  const highCharge = 0.5
 
-// Battery measurements
-const btryHeight = 120
-const btryWidth = 300
-const btryX = 54
-const btryInitY = 194
-const btryPad = 60
-const btryRad = 10
-const btryStroke = 2
-const cthdHeight = 50
-const cthdWidth = 10
-const cthdRadius = 1
-const barRadius = 3
-
-// Charge thresholds
-const lowCharge = 0.2
-const highCharge = 0.5
-
-function setup() {
-  const myCanvas = createCanvas(canvasWidth, canvasHeight)
-  myCanvas.parent('plot1')
-}
-
-function draw() {
-  background(0)
-
-  // Obtain current time
-  let date = new Date()
-  let hours = date.getHours()
-  let minutes = date.getMinutes()
-  let seconds = date.getSeconds()
-
-  // Compute remaining time
-  let secRemain = (60 - seconds) % 60
-  let minRemain = seconds > 0 ? 59 - minutes : (60 - minutes) % 60
-  let hrRemain = (seconds > 0 || minutes > 0) ? 23 - hours : 24 - hours
-
-  let timeRemain
-  if (minRemain < 10) {
-    timeRemain = `${hrRemain}:0${minRemain}`
-  } else {
-    timeRemain = `${hrRemain}:${minRemain}`
+  p.setup = function() {
+    p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
   }
 
-  // Drawing the headers
-  strokeWeight(0)
-  fill(255)
-  textSize(headerSize)
-  textFont(headerFont)
-  textAlign(CENTER)
-  text(`${timeRemain} Remaining`, canvasWidth / 2, topHeaderY)
-  text('Power Source: Battery', canvasWidth / 2, btmHeaderY)
+  p.draw = function() {
+    p.background(0)
 
-  // Drawing the hours
-  drawBattery(0, hrRemain, HOUR_PER_DAY)
+    // Obtain current time
+    let date = new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let seconds = date.getSeconds()
 
-  // Drawing the minutes
-  drawBattery(1, minRemain, MIN_PER_HOUR)
+    // Compute remaining time
+    let secRemain = (60 - seconds) % 60
+    let minRemain = seconds > 0 ? 59 - minutes : (60 - minutes) % 60
+    let hrRemain = (seconds > 0 || minutes > 0) ? 23 - hours : 24 - hours
 
-  // Drawing the second
-  drawBattery(2, secRemain, SEC_PER_MIN)
-}
+    let timeRemain
+    if (minRemain < 10) {
+      timeRemain = `${hrRemain}:0${minRemain}`
+    } else {
+      timeRemain = `${hrRemain}:${minRemain}`
+    }
 
-function drawBattery(index, amount, unit) {
-  let btryY = btryInitY + index * (btryHeight + btryPad)
-  let cthdX = btryX + btryWidth + (2 * btryStroke)
-  let cthdY = btryY + (btryHeight / 2) - (cthdHeight / 2)
+    // Drawing the headers
+    p.strokeWeight(0)
+    p.fill(255)
+    p.textSize(headerSize)
+    p.textFont(headerFont)
+    p.textAlign(p.CENTER)
+    p.text(`${timeRemain} Remaining`, CANVAS_WIDTH / 2, topHeaderY)
+    p.text('Power Source: Battery', CANVAS_WIDTH / 2, btmHeaderY)
 
-  stroke(255)
-  strokeWeight(btryStroke)
-  fill(0)
-  rect(btryX, btryY, btryWidth, btryHeight, btryRad)
+    // Drawing the hours
+    drawBattery(0, hrRemain, HOUR_PER_DAY)
 
-  fill(255)
-  rect(cthdX, cthdY, cthdWidth, cthdHeight, cthdRadius)
+    // Drawing the minutes
+    drawBattery(1, minRemain, MIN_PER_HOUR)
 
-  let charge = amount/unit
-  if (charge < lowCharge) {
-    stroke(255, 0, 0)
-    fill(255, 0, 0)
-  } else if (charge < highCharge) {
-    stroke(255, 255, 0)
-    fill(255, 255, 0)
-  } else {
-    stroke(0, 255, 0)
-    fill(0, 255, 0)
+    // Drawing the second
+    drawBattery(2, secRemain, SEC_PER_MIN)
   }
 
-  let barX = btryX + (2 * btryStroke)
-  let barY = btryY + (2 * btryStroke)
-  let barHeight = btryHeight - (4 * btryStroke)
-  let barWidth = (btryWidth - 2 * btryStroke) / unit - (2 * btryStroke)
+  function drawBattery(index, amount, unit) {
+    let btryY = btryInitY + index * (btryHeight + btryPad)
+    let cthdX = btryX + btryWidth + (2 * btryStroke)
+    let cthdY = btryY + (btryHeight / 2) - (cthdHeight / 2)
 
-  for (let i = 0; i < amount; i++) {
-    rect(barX, barY, barWidth, barHeight, barRadius)
-    barX += barWidth + (2 * btryStroke)
+    p.stroke(255)
+    p.strokeWeight(btryStroke)
+    p.fill(0)
+    p.rect(btryX, btryY, btryWidth, btryHeight, btryRad)
+
+    p.fill(255)
+    p.rect(cthdX, cthdY, cthdWidth, cthdHeight, cthdRadius)
+
+    let charge = amount/unit
+    if (charge < lowCharge) {
+      p.stroke(255, 0, 0)
+      p.fill(255, 0, 0)
+    } else if (charge < highCharge) {
+      p.stroke(255, 255, 0)
+      p.fill(255, 255, 0)
+    } else {
+      p.stroke(0, 255, 0)
+      p.fill(0, 255, 0)
+    }
+
+    let barX = btryX + (2 * btryStroke)
+    let barY = btryY + (2 * btryStroke)
+    let barHeight = btryHeight - (4 * btryStroke)
+    let barWidth = (btryWidth - 2 * btryStroke) / unit - (2 * btryStroke)
+
+    for (let i = 0; i < amount; i++) {
+      p.rect(barX, barY, barWidth, barHeight, barRadius)
+      barX += barWidth + (2 * btryStroke)
+    }
   }
 }
+
+new p5(batteryClock, 'plot1')
---
+++ Project_1/shared.js
@@ -0,0 +1,13 @@
+// Time constants
+const HOUR_PER_DAY = 24
+const MIN_PER_HOUR = 60
+const SEC_PER_MIN = 60
+
+// Angle constants
+const THETA_PER_HOUR = Math.PI / 6
+const THETA_PER_MINUTE = Math.PI / 30
+const THETA_PER_SECOND = Math.PI / 30
+
+// Canvas measurements
+const CANVAS_WIDTH = 412
+const CANVAS_HEIGHT = 734
